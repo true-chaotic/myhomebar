3bfb96b0cc04e5341fb71b806f2c2833b11608c7
Samokhin Alex <truechaoticphone@gmail.com>
2022-05-09 10:27:36 +0300

    Strict typechecks FTW

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,15 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-const container = document.getElementById('root');
+const ROOT_ID = 'root';
+
+let container = document.getElementById(ROOT_ID);
+
+if (!container) {
+    container = document.createElement('div');
+    container.id = ROOT_ID
+    document.body.appendChild(container);
+}
+
 const root = createRoot(container);
 root.render(<App />);
