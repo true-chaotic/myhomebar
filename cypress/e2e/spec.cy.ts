9584a910780e0e450ad2e916065d120b6d015236
Ekaterina <b.ekaterina098@gmail.com>
2022-08-16 20:42:49 +0300

    lint tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,34 +1,34 @@
 describe('Application', () => {
   it('starts with Cocktails page', () => {
-    cy.visit('http://localhost:1234')
-    cy.contains('Cocktails')
-    cy.contains('B-52')
-  })
+    cy.visit('http://localhost:1234');
+    cy.contains('Cocktails');
+    cy.contains('B-52');
+  });
 
   it('adds log to the Logs page', () => {
-    cy.contains('Logs').click()
-    cy.contains('Application initialized')
-  })
-})
+    cy.contains('Logs').click();
+    cy.contains('Application initialized');
+  });
+});
 
 describe('Add cocktail', () => {
   it('add Lemonade to the page', () => {
-    cy.visit('http://localhost:1234')
-    cy.contains('Cocktails')
-    cy.get('[aria-label="Add cocktail"]').click()
-    cy.get('#name').type('Lemonade')
-    cy.get('form').submit()
-    cy.contains('Lemonade')
-  })
-})
+    cy.visit('http://localhost:1234');
+    cy.contains('Cocktails');
+    cy.get('[aria-label="Add cocktail"]').click();
+    cy.get('#name').type('Lemonade');
+    cy.get('form').submit();
+    cy.contains('Lemonade');
+  });
+});
 
 describe('Add bottle', () => {
   it('add Lemon juice to the page', () => {
-    cy.visit('http://localhost:1234')
-    cy.contains('Bottles').click()
-    cy.get('[aria-label="Add bottle"]').click()
-    cy.get('#name').type('Lemon juice')
-    cy.get('form').submit()
-    cy.contains('Lemon juice')
-  })
-})
+    cy.visit('http://localhost:1234');
+    cy.contains('Bottles').click();
+    cy.get('[aria-label="Add bottle"]').click();
+    cy.get('#name').type('Lemon juice');
+    cy.get('form').submit();
+    cy.contains('Lemon juice');
+  });
+});
