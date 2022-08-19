267d42acd0af179be9d3d9142819698166b92f0a
Ekaterina <b.ekaterina098@gmail.com>
2022-08-19 21:03:22 +0300

    resolve #10:clear db before each test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,34 +1,29 @@
-describe('Application', () => {
-  it('starts with Cocktails page', () => {
-    cy.visit('/');
-    cy.contains('Cocktails');
-    cy.contains('B-52');
-  });
+import Dexie from 'dexie';
 
-  it('adds log to the Logs page', () => {
-    cy.contains('Logs').click();
-    cy.contains('Application initialized');
-  });
+beforeEach(() => { Dexie.delete('bar'); });
+
+it('starts with Cocktails page', () => {
+  cy.visit('/');
+  cy.contains('Cocktails');
+  cy.contains('B-52');
+  cy.contains('Logs').click();
+  cy.contains('Application initialized');
 });
 
-describe('Add cocktail', () => {
-  it('add Lemonade to the page', () => {
-    cy.visit('/');
-    cy.contains('Cocktails');
-    cy.get('[aria-label="Add cocktail"]').click();
-    cy.get('#name').type('Lemonade');
-    cy.get('form').submit();
-    cy.contains('Lemonade');
-  });
+it('add Lemonade to the page', () => {
+  cy.visit('/');
+  cy.contains('Cocktails');
+  cy.get('[aria-label="Add cocktail"]').click();
+  cy.get('#name').type('Lemonade');
+  cy.get('form').submit();
+  cy.contains('Lemonade');
 });
 
-describe('Add bottle', () => {
-  it('add Lemon juice to the page', () => {
-    cy.visit('/');
-    cy.contains('Bottles').click();
-    cy.get('[aria-label="Add bottle"]').click();
-    cy.get('#name').type('Lemon juice');
-    cy.get('form').submit();
-    cy.contains('Lemon juice');
-  });
+it('add Lemon juice to the page', () => {
+  cy.visit('/');
+  cy.contains('Bottles').click();
+  cy.get('[aria-label="Add bottle"]').click();
+  cy.get('#name').type('Lemon juice');
+  cy.get('form').submit();
+  cy.contains('Lemon juice');
 });
