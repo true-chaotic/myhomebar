c2f7f5f26527f3375e5b87a3233e34d373222715
Ekaterina <b.ekaterina098@gmail.com>
2022-08-30 21:04:45 +0300

    add aliases for volume inputs

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -65,31 +65,37 @@ it('changing the volume of the bottle', () => {
     .should('eq', 'true');
 
   cy.get('[data-cy="add-bottle-button"]').click();
+
+  cy.get('[name="total-volume"]').as('total');
+  cy.get('[name="current-volume"]').as('current');
+
   // value synchronization
-  cy.get('[name="total-volume"]').select('1000').should('have.value', '1000');
-  cy.get('[name="current-volume"]').should('have.value', '1000');
+  cy.get('@total').select('1000').should('have.value', '1000');
+  cy.get('@current').should('have.value', '1000');
 
-  cy.get('[name="total-volume"]').select('750').should('have.value', '750');
-  cy.get('[name="current-volume"]').should('have.value', '750');
+  cy.get('@total').select('750').should('have.value', '750');
+  cy.get('@current').should('have.value', '750');
 
-  cy.get('[name="total-volume"]').select('500').should('have.value', '500');
-  cy.get('[name="current-volume"]').should('have.value', '500');
+  cy.get('@total').select('500').should('have.value', '500');
+  cy.get('@current').should('have.value', '500');
 
-  cy.get('[name="total-volume"]').select('750').should('have.value', '750');
-  cy.get('[name="current-volume"]').should('have.value', '750');
+  cy.get('@total').select('750').should('have.value', '750');
+  cy.get('@current').should('have.value', '750');
 
-  cy.get('[name="total-volume"]').select('1000').should('have.value', '1000');
-  cy.get('[name="current-volume"]').should('have.value', '1000');
+  cy.get('@total').select('1000').should('have.value', '1000');
+  cy.get('@current').should('have.value', '1000');
 
   // value asynchronization
-  cy.get('[name="total-volume"]').select('500').should('have.value', '500');
-  cy.get('[name="current-volume"]').should('have.value', '500');
-  cy.get('[name="current-volume"]').type('{backspace}{backspace}{backspace}');
-  cy.get('[name="current-volume"]').should('have.value', '0');
-  cy.get('[name="current-volume"]').type('400');
-  cy.get('[name="current-volume"]').should('have.value', '400');
-  cy.get('[name="total-volume"]').select('750').should('have.value', '750');
-  cy.get('[name="current-volume"]').should('have.value', '400');
-  cy.get('[name="current-volume"]').type('4000');
-  cy.get('[name="current-volume"]').should('have.value', '750');
+  cy.get('@total').select('500').should('have.value', '500');
+  cy.get('@current').should('have.value', '500');
+
+  cy.get('@current').type('{backspace}{backspace}{backspace}');
+  cy.get('@current').should('have.value', '0');
+  cy.get('@current').type('400');
+  cy.get('@current').should('have.value', '400');
+
+  cy.get('@total').select('750').should('have.value', '750');
+  cy.get('@current').should('have.value', '400');
+  cy.get('@current').type('4000');
+  cy.get('@current').should('have.value', '750');
 });
