837a535fec766864d231323175a85b73fc2ab03f
Ekaterina <b.ekaterina098@gmail.com>
2022-09-15 17:43:47 +0300

    add close the form tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -64,8 +64,8 @@ it('changing the volume of the bottle', () => {
 
   cy.get('[data-cy="add-bottle-button"]').click();
 
-  cy.get('[name="total-volume"]').as('total');
-  cy.get('[name="current-volume"]').as('current');
+  cy.get('[data-cy="total-volume"]').as('total');
+  cy.get('[data-cy="current-volume"]').as('current');
 
   // value synchronization
   cy.get('@total').select('1000').should('have.value', '1000');
@@ -96,4 +96,66 @@ it('changing the volume of the bottle', () => {
   cy.get('@current').should('have.value', '400');
   cy.get('@current').type('4000');
   cy.get('@current').should('have.value', '750');
+});
+
+it('close the form', () => {
+  cy.visit('/');
+  cy.contains('Cocktails')
+    .invoke('attr', 'aria-selected')
+    .should('eq', 'true');
+  cy.get('[data-cy="add-cocktail-button"]').click();
+  cy.get('[data-cy="cocktail-name"]').type('Lemonade');
+  cy.get('[data-cy="cancel-button-cocktail"]').click();
+  cy.get('[data-cy="add-cocktail-button"]').click();
+  cy.get('[data-cy="cocktail-name"]').should('have.value', 'Lemonade');
+
+  cy.visit('/');
+  cy.contains('Bottles').click()
+    .invoke('attr', 'aria-selected')
+    .should('eq', 'true');
+  cy.get('[data-cy="add-bottle-button"]').click();
+  cy.contains('Add new bottle');
+  cy.contains('Bottle name');
+  cy.contains('Type');
+  cy.contains('Total volume');
+  cy.contains('Current volume');
+  cy.get('[data-cy="bottle-name"]').type('Lemon juice');
+  cy.get('[data-cy="total-volume"]').select('500').should('have.value', '500');
+  cy.get('[data-cy="current-volume"]').should('have.value', '500');
+  cy.get('[data-cy="cancel-button-bottle"]').click();
+  cy.get('[data-cy="add-bottle-button"]').click();
+  cy.get('[data-cy="bottle-name"]').should('have.value', 'Lemon juice');
+  cy.get('[data-cy="total-volume"]').should('have.value', '500');
+  cy.get('[data-cy="current-volume"]').should('have.value', '500');
+
+  // form submission
+  cy.visit('/');
+  cy.contains('Cocktails')
+    .invoke('attr', 'aria-selected')
+    .should('eq', 'true');
+  cy.get('[data-cy="add-cocktail-button"]').click();
+  cy.get('[data-cy="cocktail-name"]').type('Lemonade');
+  cy.get('form').submit();
+  cy.contains('Lemonade');
+  cy.get('[data-cy="add-cocktail-button"]').click();
+  cy.get('[data-cy="cocktail-name"]').should('have.value', '');
+
+  cy.visit('/');
+  cy.contains('Bottles').click()
+    .invoke('attr', 'aria-selected')
+    .should('eq', 'true');
+  cy.get('[data-cy="add-bottle-button"]').click();
+  cy.contains('Add new bottle');
+  cy.contains('Bottle name');
+  cy.contains('Type');
+  cy.contains('Total volume');
+  cy.contains('Current volume');
+  cy.get('[data-cy="bottle-name"]').type('Lemon juice');
+  cy.get('[data-cy="total-volume"]').select('500').should('have.value', '500');
+  cy.get('[data-cy="current-volume"]').should('have.value', '500');
+  cy.get('form').submit();
+  cy.get('[data-cy="add-bottle-button"]').click();
+  cy.get('[data-cy="bottle-name"]').should('have.value', '');
+  cy.get('[data-cy="total-volume"]').should('have.value', '750');
+  cy.get('[data-cy="current-volume"]').should('have.value', '750');
 });
